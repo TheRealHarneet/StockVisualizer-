ca9b1fdcfd76eff0a4bd5e4c3875ea8f0b651211
Harneetchanni <Harneetsinghchanni99@gmail.com>
2021-06-29 23:48:00 -0400

    Removed Testing Prints & Added Comments

--- graphvisual.py
+++ graphvisual.py
@@ -12,7 +12,7 @@ style.use('ggplot')
 
 STOCK_TICKER = input("Please Enter Stock Symbol (Ticker)")
 
-# Intialize Dates for Desired Stock 
+# Intialize Dates for stock using user input
 
 start_year, start_month, start_day = input("Enter Year,Month,Date:").split(',')
 end_year, end_month, end_day = input("Enter Year,Month,Date:").split(',')
@@ -27,7 +27,7 @@ csv_name = STOCK_TICKER.lower() + '.csv'
 # Check if csv already exists if so, overwrite or append, else create a new csv
 
 if path.exists(csv_name):
-  # O - Overwrite , A - Append , N - Neither 
+  # O - Overwrite , A - Append , N - Neither(Just Passes Through)
   override_or_append = input("Would you like to Overwrite, Append or Neither? O or A or N")
   if override_or_append == "O":
     stock_vars.to_csv(csv_name)
@@ -36,14 +36,13 @@ if path.exists(csv_name):
 else:
   stock_vars.to_csv(csv_name) 
 
-#Read in CSV 
+#Read in CSV created from above 
 
 df_inital = pd.read_csv(csv_name,parse_dates=True, index_col = 0)
-print(len(df_inital))
 
 #Remove Duplicates 
 df_unique = (df_inital.drop_duplicates()).copy()
-print(len(df_unique))
+
 
 # Using MplFinance, Create Candlestick Graph 
 mpf.plot(df_unique, type = "candle", volume= True, mav = 2)
@@ -62,7 +61,10 @@ ax1 = plt.subplot2grid((6,1),(0,0), rowspan = 5, colspan =1 )
 ax1.plot(df_unique.index, df_unique['Adj Close'] , label = 'Adj Close')
 ax1.plot(df_unique.index, df_unique['20ma'], label = "Moving Average")
 
+#Added Graph Legend + Axis Description 
 ax1.legend(loc='best')
 ax1.set_ylabel('Price in $')
 
+
+#Ouputs plot's for Adj Close & Moving Average 
 plt.show()
